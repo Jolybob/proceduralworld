2ccb4d9ba25cc2d5771a0a264d8e9434c1d38790
Jolybob <ohbelisque@gmail.com>
2026-09-17 01:34:58 +0200

    Fix ResourceId ownership in resource catalog

--- Runtime/Generation/Resources/ResourceTypes.cs
+++ Runtime/Generation/Resources/ResourceTypes.cs
@@ -3,21 +3,6 @@ using System.Collections.Generic;
 
 namespace Jolybob.ProceduralWorld
 {
-    /// <summary>Stable identifier for generated resource types.</summary>
-    public readonly struct ResourceId : IEquatable<ResourceId>
-    {
-        public readonly byte Value;
-
-        public ResourceId(byte value) => Value = value;
-
-        public bool Equals(ResourceId other) => Value == other.Value;
-        public override bool Equals(object obj) => obj is ResourceId other && Equals(other);
-        public override int GetHashCode() => Value.GetHashCode();
-        public static bool operator ==(ResourceId left, ResourceId right) => left.Equals(right);
-        public static bool operator !=(ResourceId left, ResourceId right) => !left.Equals(right);
-        public override string ToString() => Value.ToString();
-    }
-
     /// <summary>
     /// Data-only description of a resource that may spawn in generated terrain.
     /// </summary>

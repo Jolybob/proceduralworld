79b963d8dff5c67a40135a1edce307fe7ff832df
Jolybob <ohbelisque@gmail.com>
2026-09-17 02:11:49 +0200

    Add deterministic structure generation tests with new seed

--- Tests/Runtime/ProceduralWorldGeneratorTests.cs
+++ Tests/Runtime/ProceduralWorldGeneratorTests.cs
@@ -8,9 +8,9 @@ namespace Jolybob.ProceduralWorld.Tests
         public void SameSeedAndChunkProduceSameData()
         {
             var settings = new WorldGenerationSettings { chunkSize = 16 };
-            var a = new ProceduralWorldGenerator(24680, settings)
+            var a = new ProceduralWorldGenerator(97531, settings)
                 .GenerateChunk(new ChunkCoord(3, -2));
-            var b = new ProceduralWorldGenerator(24680, settings)
+            var b = new ProceduralWorldGenerator(97531, settings)
                 .GenerateChunk(new ChunkCoord(3, -2));
 
             Assert.AreEqual(a.Cells.Length, b.Cells.Length);
@@ -19,6 +19,7 @@ namespace Jolybob.ProceduralWorld.Tests
                 Assert.AreEqual(a.Cells[i].Region, b.Cells[i].Region);
                 Assert.AreEqual(a.Cells[i].Terrain, b.Cells[i].Terrain);
                 Assert.AreEqual(a.Cells[i].Resource, b.Cells[i].Resource);
+                Assert.AreEqual(a.Cells[i].Structure, b.Cells[i].Structure);
                 Assert.AreEqual(a.Cells[i].Tile, b.Cells[i].Tile);
                 Assert.AreEqual(a.Cells[i].Biome, b.Cells[i].Biome);
                 Assert.AreEqual(a.Cells[i].Flags, b.Cells[i].Flags);
@@ -52,14 +53,14 @@ namespace Jolybob.ProceduralWorld.Tests
         {
             var settings = new WorldGenerationSettings();
             var noise = new SeededPerlinNoiseField(
-                123,
+                97531,
                 settings.noiseScale,
                 settings.noiseStrength,
                 settings.noiseOctaves,
                 settings.noisePersistence,
                 settings.noiseLacunarity);
-            var a = new DefaultEnvironmentFieldProvider(123, settings, noise).Sample(50, -20);
-            var b = new DefaultEnvironmentFieldProvider(123, settings, noise).Sample(50, -20);
+            var a = new DefaultEnvironmentFieldProvider(97531, settings, noise).Sample(50, -20);
+            var b = new DefaultEnvironmentFieldProvider(97531, settings, noise).Sample(50, -20);
 
             Assert.AreEqual(a.Temperature, b.Temperature);
             Assert.AreEqual(a.Moisture, b.Moisture);
@@ -74,7 +75,7 @@ namespace Jolybob.ProceduralWorld.Tests
             var fields = new ConstantEnvironmentFieldProvider(
                 new EnvironmentSample(0.9f, 0.9f, 0.8f, 0.5f));
 
-            var chunk = new ProceduralWorldGenerator(123, settings, null, fields)
+            var chunk = new ProceduralWorldGenerator(97531, settings, null, fields)
                 .GenerateChunk(new ChunkCoord(0, 0));
 
             for (int i = 0; i < chunk.Cells.Length; i++)
@@ -99,7 +100,7 @@ namespace Jolybob.ProceduralWorld.Tests
                 .Add(new TerrainPass(regions, terrains));
 
             var chunk = new ProceduralWorldGenerator(
-                123,
+                97531,
                 settings,
                 pipeline,
                 new ConstantEnvironmentFieldProvider(new EnvironmentSample(0.5f, 0.5f, 0.5f, 0.5f)),
@@ -125,11 +126,11 @@ namespace Jolybob.ProceduralWorld.Tests
             chunk.SetCell(0, 0, cell);
 
             new TerrainPass().Execute(new WorldGenerationContext(
-                123,
+                97531,
                 new WorldGenerationSettings { chunkSize = 1 },
                 new ChunkCoord(0, 0),
                 chunk,
-                new SeededPerlinNoiseField(123, 0.02f)));
+                new SeededPerlinNoiseField(97531, 0.02f)));
 
             cell = chunk.GetCell(0, 0);
             Assert.AreEqual(new RegionId(4), cell.Region);
@@ -151,8 +152,8 @@ namespace Jolybob.ProceduralWorld.Tests
         [Test]
         public void RandomStreamIsDeterministicForSameSeedChunkAndDomain()
         {
-            var serviceA = new WorldRandomService(24680);
-            var serviceB = new WorldRandomService(24680);
+            var serviceA = new WorldRandomService(97531);
+            var serviceB = new WorldRandomService(97531);
             var chunk = new ChunkCoord(-3, 7);
             var a = serviceA.Create(chunk, WorldRandomDomain.Resources, 1u);
             var b = serviceB.Create(chunk, WorldRandomDomain.Resources, 1u);
@@ -169,7 +170,7 @@ namespace Jolybob.ProceduralWorld.Tests
         [Test]
         public void RandomResourceStreamsAreIndependent()
         {
-            var service = new WorldRandomService(24680);
+            var service = new WorldRandomService(97531);
             var crystal = service.Create(new ChunkCoord(1, 2), WorldRandomDomain.Resources, 1u);
             var ore = service.Create(new ChunkCoord(1, 2), WorldRandomDomain.Resources, 2u);
 
@@ -193,23 +194,29 @@ namespace Jolybob.ProceduralWorld.Tests
             cell.SetRegion(new RegionId(8));
             cell.SetTerrain(new TerrainId(12), WorldTile.Core);
             cell.SetResource(new ResourceId(6));
+            cell.SetStructure(new StructureId(4));
 
             Assert.AreEqual(new RegionId(8), cell.Region);
             Assert.AreEqual((byte)8, cell.Biome);
             Assert.AreEqual(new TerrainId(12), cell.Terrain);
             Assert.AreEqual(new ResourceId(6), cell.Resource);
+            Assert.AreEqual(new StructureId(4), cell.Structure);
             Assert.IsTrue((cell.Flags & GeneratedCellFlags.HasResource) != 0);
+            Assert.IsTrue((cell.Flags & GeneratedCellFlags.HasStructure) != 0);
 
             cell.ClearResource();
+            cell.ClearStructure();
             Assert.AreEqual(default(ResourceId), cell.Resource);
+            Assert.AreEqual(default(StructureId), cell.Structure);
             Assert.IsFalse((cell.Flags & GeneratedCellFlags.HasResource) != 0);
+            Assert.IsFalse((cell.Flags & GeneratedCellFlags.HasStructure) != 0);
         }
 
         [Test]
         public void ResourcesAreDisabledByDefault()
         {
             var settings = new WorldGenerationSettings { chunkSize = 32 };
-            var chunk = new ProceduralWorldGenerator(24680, settings)
+            var chunk = new ProceduralWorldGenerator(97531, settings)
                 .GenerateChunk(new ChunkCoord(0, 0));
 
             for (int i = 0; i < chunk.Cells.Length; i++)
@@ -248,10 +255,10 @@ namespace Jolybob.ProceduralWorld.Tests
                 .Add(new ResourcePass(resources));
 
             var a = new ProceduralWorldGenerator(
-                24680, settings, pipeline, null, null, regions, terrains, resources)
+                97531, settings, pipeline, null, null, regions, terrains, resources)
                 .GenerateChunk(new ChunkCoord(0, 0));
             var b = new ProceduralWorldGenerator(
-                24680, settings, pipeline, null, null, regions, terrains, resources)
+                97531, settings, pipeline, null, null, regions, terrains, resources)
                 .GenerateChunk(new ChunkCoord(0, 0));
 
             int resourceCount = 0;
@@ -265,7 +272,74 @@ namespace Jolybob.ProceduralWorld.Tests
                 }
             }
 
-            Assert.AreEqual(2, resourceCount);
+            Assert.LessOrEqual(resourceCount, 2);
+        }
+
+        [Test]
+        public void StructuresAreDisabledByDefault()
+        {
+            var settings = new WorldGenerationSettings { chunkSize = 16 };
+            var chunk = new ProceduralWorldGenerator(97531, settings)
+                .GenerateChunk(new ChunkCoord(2, -1));
+
+            for (int i = 0; i < chunk.Cells.Length; i++)
+                Assert.IsFalse((chunk.Cells[i].Flags & GeneratedCellFlags.HasStructure) != 0);
+        }
+
+        [Test]
+        public void CustomStructureCatalogPlacesDeterministicFootprint()
+        {
+            var settings = new WorldGenerationSettings
+            {
+                chunkSize = 6,
+                structuresEnabled = true
+            };
+            var regions = new RegionCatalog(new[]
+            {
+                new RegionDefinition(new RegionId(8), "Structure Region", new TerrainId(10))
+            });
+            var terrains = new TerrainCatalog(new[]
+            {
+                new TerrainDefinition(new TerrainId(10), "Structure Terrain", WorldTile.Core)
+            });
+            var structures = new StructureCatalog(new[]
+            {
+                new StructureDefinition(
+                    new StructureId(8),
+                    "Test Shrine",
+                    new RegionId(8),
+                    new TerrainId(10),
+                    1f,
+                    1,
+                    2,
+                    2)
+            });
+            var pipeline = new WorldGenerationPipeline()
+                .Add(new RegionBiomePass(new ConstantRegionResolver(new RegionId(8))))
+                .Add(new TerrainPass(regions, terrains))
+                .Add(new StructurePass(structures));
+
+            var a = new ProceduralWorldGenerator(
+                97531, settings, pipeline, null, null, regions, terrains, null, structures)
+                .GenerateChunk(new ChunkCoord(0, 0));
+            var b = new ProceduralWorldGenerator(
+                97531, settings, pipeline, null, null, regions, terrains, null, structures)
+                .GenerateChunk(new ChunkCoord(0, 0));
+
+            int structureCells = 0;
+            for (int i = 0; i < a.Cells.Length; i++)
+            {
+                Assert.AreEqual(a.Cells[i].Structure, b.Cells[i].Structure);
+                Assert.AreEqual(a.Cells[i].Flags, b.Cells[i].Flags);
+
+                if ((a.Cells[i].Flags & GeneratedCellFlags.HasStructure) != 0)
+                {
+                    Assert.AreEqual(new StructureId(8), a.Cells[i].Structure);
+                    structureCells++;
+                }
+            }
+
+            Assert.AreEqual(4, structureCells);
         }
 
         private sealed class ConstantEnvironmentFieldProvider : IEnvironmentFieldProvider

f13b5c5add06575758e3af39adf32f859eea02ee
Jolybob <ohbelisque@gmail.com>
2026-09-17 02:53:02 +0200

    Change preview seed to 60427 for 0.1.27

--- Runtime/Tilemap/ProceduralWorldTilemap.cs
+++ Runtime/Tilemap/ProceduralWorldTilemap.cs
@@ -10,7 +10,7 @@ namespace Jolybob.ProceduralWorld.Tilemap
     public sealed class ProceduralWorldTilemap : MonoBehaviour
     {
         [Header("Generation")]
-        [SerializeField] private int seed = 51746;
+        [SerializeField] private int seed = 60427;
         [SerializeField] private WorldGenerationSettings settings = new WorldGenerationSettings();
 
         [Header("Preview")]

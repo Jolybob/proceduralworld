4332122857c34b900e98de57fd00d43d740eeda0
Jolybob <ohbelisque@gmail.com>
2026-09-17 05:40:40 +0200

    Keep Tilemap renderer on resolver abstraction

--- Runtime/Tilemap/WorldTilemapRenderer.cs
+++ Runtime/Tilemap/WorldTilemapRenderer.cs
@@ -21,10 +21,7 @@ namespace Jolybob.ProceduralWorld.Tilemap
         public int ChunkSize => chunkSize;
         public IWorldCellVisualResolver VisualResolver => visualResolver;
 
-        public WorldTilemapRenderer(
-            UnityTilemap tilemap,
-            int chunkSize,
-            IReadOnlyDictionary<WorldTile, TileBase> tiles)
+        public WorldTilemapRenderer(UnityTilemap tilemap, int chunkSize, IReadOnlyDictionary<WorldTile, TileBase> tiles)
             : this(tilemap, chunkSize, new WorldCellVisualCatalog(tiles))
         {
         }

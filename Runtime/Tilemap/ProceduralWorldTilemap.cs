03c673a969e8926a4d78241514636aadf93c90ce
Jolybob <ohbelisque@gmail.com>
2026-09-17 02:45:12 +0200

    Change preview seed for persistence architecture

--- Runtime/Tilemap/ProceduralWorldTilemap.cs
+++ Runtime/Tilemap/ProceduralWorldTilemap.cs
@@ -10,7 +10,7 @@ namespace Jolybob.ProceduralWorld.Tilemap
     public sealed class ProceduralWorldTilemap : MonoBehaviour
     {
         [Header("Generation")]
-        [SerializeField] private int seed = 59273;
+        [SerializeField] private int seed = 43017;
         [SerializeField] private WorldGenerationSettings settings = new WorldGenerationSettings();
 
         [Header("Preview")]

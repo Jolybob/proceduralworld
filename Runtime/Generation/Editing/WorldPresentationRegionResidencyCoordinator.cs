43db71f833fb2f4cb0e8ecdaa08e77288fb39046
Jolybob <ohbelisque@gmail.com>
2026-09-17 07:25:24 +0200

    Expose deterministic region residency snapshot

--- Runtime/Generation/Editing/WorldPresentationRegionResidencyCoordinator.cs
+++ Runtime/Generation/Editing/WorldPresentationRegionResidencyCoordinator.cs
@@ -12,24 +12,18 @@ namespace Jolybob.ProceduralWorld
 
         public bool IsDisposed => disposed;
         public int LoadedRegionCount => loadedRegions.Count;
+        public IReadOnlyList<int> LoadedRegions => loadOrder;
 
         public WorldPresentationRegionResidencyCoordinator(IWorldPresentationRegionLifecycle lifecycle)
         {
             this.lifecycle = lifecycle ?? throw new ArgumentNullException(nameof(lifecycle));
         }
 
-        public bool IsLoaded(int regionId)
-        {
-            return loadedRegions.Contains(regionId);
-        }
+        public bool IsLoaded(int regionId) => loadedRegions.Contains(regionId);
 
         public bool EnsureLoaded(int regionId)
         {
-            if (disposed || !loadedRegions.Add(regionId))
-            {
-                return false;
-            }
-
+            if (disposed || !loadedRegions.Add(regionId)) return false;
             loadOrder.Add(regionId);
             lifecycle.LoadRegion(regionId);
             return true;
@@ -37,11 +31,7 @@ namespace Jolybob.ProceduralWorld
 
         public bool EnsureUnloaded(int regionId)
         {
-            if (disposed || !loadedRegions.Remove(regionId))
-            {
-                return false;
-            }
-
+            if (disposed || !loadedRegions.Remove(regionId)) return false;
             loadOrder.Remove(regionId);
             lifecycle.UnloadRegion(regionId);
             return true;
@@ -49,27 +39,15 @@ namespace Jolybob.ProceduralWorld
 
         public void Clear()
         {
-            if (disposed)
-            {
-                return;
-            }
-
-            for (var i = loadOrder.Count - 1; i >= 0; i--)
-            {
-                lifecycle.UnloadRegion(loadOrder[i]);
-            }
-
+            if (disposed) return;
+            for (var i = loadOrder.Count - 1; i >= 0; i--) lifecycle.UnloadRegion(loadOrder[i]);
             loadOrder.Clear();
             loadedRegions.Clear();
         }
 
         public void Dispose()
         {
-            if (disposed)
-            {
-                return;
-            }
-
+            if (disposed) return;
             disposed = true;
             loadOrder.Clear();
             loadedRegions.Clear();

72c8df9e4aadf67f231cb6a8494f4cca95116427
Jolybob <ohbelisque@gmail.com>
2026-09-17 06:24:15 +0200

    Add state-driven visual overlay layer

---
+++ Runtime/Tilemap/WorldCellVisualStateOverlayLayer.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+
+namespace Jolybob.ProceduralWorld.Tilemap
+{
+    public sealed class WorldCellVisualStateOverlayLayer : IWorldCellVisualOverlayContextLayer
+    {
+        private readonly IWorldCellVisualStateProvider stateProvider;
+        private readonly IReadOnlyDictionary<WorldCellVisualState, TileBase> tiles;
+
+        public WorldCellVisualStateOverlayLayer(
+            IWorldCellVisualStateProvider stateProvider,
+            IReadOnlyDictionary<WorldCellVisualState, TileBase> tiles,
+            int order = 100)
+        {
+            this.stateProvider = stateProvider ?? throw new ArgumentNullException(nameof(stateProvider));
+            this.tiles = tiles ?? throw new ArgumentNullException(nameof(tiles));
+            if (order < 0)
+                throw new ArgumentOutOfRangeException(nameof(order));
+            Order = order;
+        }
+
+        public int Order { get; }
+
+        public bool TryResolve(GeneratedCell cell, out TileBase tile)
+        {
+            tile = null;
+            return false;
+        }
+
+        public bool TryResolve(WorldPosition position, GeneratedCell cell, out TileBase tile)
+        {
+            return tiles.TryGetValue(stateProvider.GetState(position, cell), out tile);
+        }
+    }
+}
